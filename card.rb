65783fa4086452a3dc53e28e924d87f125ca2c11
metacoder87 <gary.miller@gmx.com>
2020-06-28 22:11:15 -0400

    Adds in to_s and == methods

--- card.rb
+++ card.rb
@@ -1,6 +1,14 @@
 class Card
 
+    def to_s
+        self.to_s
+    end
 
+    def ==(b)
+        self.eql? b
+    end
+
+    attr_reader :face_val
 
     def initialize(face_val)
         @face_val = face_val
