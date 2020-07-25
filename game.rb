76a68f67fde2e591e754699201ca41b58bfe643f
metacoder87 <gary.miller@gmx.com>
2020-07-24 21:16:23 -0400

    Creates the Game class

---
+++ game.rb
@@ -0,0 +1,8 @@
+            # meta_coder (Gary Miller) =)
+            # gmiller052611@gmail.com
+            # https://github.com/metacoder87/memory_puzzle
+
+
+require_relative "board"
+
+class Game
