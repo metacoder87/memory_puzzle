10155318e1a79a7706da31afcf80d2b4ba6d8556
metacoder87 <gary.miller@gmx.com>
2020-07-26 04:47:05 -0400

    Creates and initializes the HumanPlayer class

---
+++ human_player.rb
@@ -0,0 +1,10 @@
+            # meta_coder (Gary Miller) =)
+            # gmiller052611@gmail.com
+            # https://github.com/metacoder87/memory_puzzle
+
+
+class HumanPlayer
+
+    def initialize(player)
+        @player = player
+    end
