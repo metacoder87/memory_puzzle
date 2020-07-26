c5a237290a91aa849200a495540e98e9b80fdd74
metacoder87 <gary.miller@gmx.com>
2020-07-26 07:38:57 -0400

    Creates the ComputerPlayer class

---
+++ computer_player.rb
@@ -0,0 +1,20 @@
+            # meta_coder (Gary Miller) =)
+            # gmiller052611@gmail.com
+            # https://github.com/metacoder87/memory_puzzle
+
+
+require_relative "board"
+           
+class ComputerPlayer
+
+    def initialize(player)
+        @player = player
+        @memo = {}
+    end
+
+end
+
+
+            # meta_coder (Gary Miller) =)
+            # gmiller052611@gmail.com
+            # https://github.com/metacoder87/memory_puzzle
