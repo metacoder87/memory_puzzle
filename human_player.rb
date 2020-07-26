796d1ff069bb9bd5d252438dcc4337771e763bfc
metacoder87 <gary.miller@gmx.com>
2020-07-26 04:47:31 -0400

    Adds the get_guess method into HumanPlayer

--- human_player.rb
+++ human_player.rb
@@ -8,3 +8,16 @@ class HumanPlayer
     def initialize(player)
         @player = player
     end
+
+    def get_guess
+        puts "Take a guess..."
+        move = gets.chomp.split(" ")
+        move.map! { |ele| ele.to_i }
+    end
+
+end
+
+
+            # meta_coder (Gary Miller) =)
+            # gmiller052611@gmail.com
+            # https://github.com/metacoder87/memory_puzzle
