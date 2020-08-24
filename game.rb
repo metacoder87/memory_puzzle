20d20859eee96999b3c3d26c639e47e86daea5be
metacoder87 <gary.miller@gmx.com>
2020-08-24 14:13:44 -0400

    Adds in the board comp bridge still needs work

--- game.rb
+++ game.rb
@@ -22,7 +22,11 @@ attr_reader :board
             system 'clear'
             @board.render(@board.display_grid)
             @board.render(@board.temp_grid)
-            @board.add_guess(@player.get_guess)
+            guess = @player.get_guess
+            puts guess
+            @board.add_guess(guess)
+            puts "#{guess} from game"
+            @player.recieve_revealed_card(@board.comp_card_feeder(guess), guess)
             @board.reveal
         end
     end
